6d6d4ed92eec3e683c5b13a9bece2338628bfafa
talha073 <talhajavedd@gmail.com>
2023-06-06 14:59:04 +0400

    add function and struct

--- contracts/amazon.sol
+++ contracts/amazon.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Amazon {
-    string public name;
     address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string catagory;
+        string image;
+        uint256 cont;
+        uint256 rating;
+        uint256 stock;
+    }
     
     constructor(){
-        name = "Amazon";
         owner = msg.sender;
     }
+
+    function lsit(uint256 _id, string memory _name, string memory _catagory, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
+        
+     }
 }
--- test/amazon.test.js
+++ test/amazon.test.js
@@ -15,15 +15,10 @@ describe("Amazon", () => {
     // Deploy contracts
     const Amazon = await ethers.getContractFactory("Amazon");
     amazon = await Amazon.deploy();
-    // console.log("address: ", amazon.address);
   });
   describe("Development", () => {
     it("Sets the owner", async () => {
       expect(await amazon.owner()).to.equal(deployer.address);
-    });
-    it("Has a name", async () => {
-      const name = await amazon.name();
-      expect(name).to.equal("Amazon");
     });
   });
 });
