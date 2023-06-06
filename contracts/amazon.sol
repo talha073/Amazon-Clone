5a38a9221640c6682882e3f5f95407b245315c17
talha073 <talhajavedd@gmail.com>
2023-06-06 16:51:27 +0400

    adding events and complete list function

--- contracts/amazon.sol
+++ contracts/amazon.sol
@@ -7,21 +7,23 @@ contract Amazon {
     struct Item {
         uint256 id;
         string name;
-        string catagory;
+        string category;
         string image;
         uint256 cost;
         uint256 rating;
         uint256 stock;
     }
     mapping (uint256 => Item) public items;
+    event List(string name, uint256 cost, uint256 quantity);
     
     constructor(){
         owner = msg.sender;
     }
 
-    function list(uint256 _id, string memory _name, string memory _catagory, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
-        Item memory item = Item(_id, _name, _catagory, _image, _cost, _rating, _stock );
+    function list(uint256 _id, string memory _name, string memory _category, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
+        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock );
         items[_id] = item; 
+        emit List(_name, _cost, _stock);
         
      }
 }
