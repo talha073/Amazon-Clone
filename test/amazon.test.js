97417a00abe94f70d905b8c713e414c50cc6e82f
talha073 <talhajavedd@gmail.com>
2023-06-06 16:52:13 +0400

    test return attributes

--- test/amazon.test.js
+++ test/amazon.test.js
@@ -4,6 +4,13 @@ const { ethers } = require("hardhat");
 const token = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE = "IMAGE";
+const COST = token(1);
+const RATING = 4;
+const STOCK = 5;
 
 describe("Amazon", () => {
   let amazon;
@@ -24,15 +31,22 @@ describe("Amazon", () => {
 
   describe("Listing", () => {
     let transaction;
+
     beforeEach(async () => {
       transaction = await amazon
         .connect(deployer)
-        .list(1, "Shoes", "Clothing", "IMAGE", 1, 4, 5);
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
       await transaction.wait();
     });
     it("Return item attributes", async () => {
       const item = await amazon.items(1);
       expect(item.id).to.equal(1);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
     });
   });
 });
