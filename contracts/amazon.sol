cca8377ce602a4a2731d3791f4bd3897c3ba6127
talha073 <talhajavedd@gmail.com>
2023-06-06 15:20:02 +0400

    add list function

--- contracts/amazon.sol
+++ contracts/amazon.sol
@@ -9,16 +9,19 @@ contract Amazon {
         string name;
         string catagory;
         string image;
-        uint256 cont;
+        uint256 cost;
         uint256 rating;
         uint256 stock;
     }
+    mapping (uint256 => Item) public items;
     
     constructor(){
         owner = msg.sender;
     }
 
-    function lsit(uint256 _id, string memory _name, string memory _catagory, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
+    function list(uint256 _id, string memory _name, string memory _catagory, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
+        Item memory item = Item(_id, _name, _catagory, _image, _cost, _rating, _stock );
+        items[_id] = item; 
         
      }
 }
