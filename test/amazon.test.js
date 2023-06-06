723be75ee2f9fccf742ae4129b3cb9c10d8944c4
talha073 <talhajavedd@gmail.com>
2023-06-06 13:51:22 +0400

    test setup

--- test/amazon.test.js
+++ test/amazon.test.js
@@ -6,10 +6,21 @@ const token = (n) => {
 };
 
 describe("Amazon", () => {
-  it("Has a name", async () => {
-    const amazon = await ethers.getContractFactory("Amazon");
-    const amazonContract = await amazon.deploy();
-    const name = await amazonContract.name();
-    expect(name).to.equal("Amazon");
+  let amazon;
+  beforeEach(async () => {
+    //setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    // Deploy contracts
+    const Amazon = await ethers.getContractFactory("Amazon");
+    amazon = await Amazon.deploy();
+  });
+  describe("Development", () => {
+    it("Has a name", async () => {
+      const amazon = await ethers.getContractFactory("Amazon");
+      const amazonContract = await amazon.deploy();
+      const name = await amazonContract.name();
+      expect(name).to.equal("Amazon");
+    });
   });
 });
