25856d4cfc7c1d1370f9d8e4c366d3fcb9c6b842
talha073 <talhajavedd@gmail.com>
2023-06-07 16:36:41 +0400

    update

--- contracts/amazon.sol
+++ contracts/amazon.sol
@@ -16,14 +16,21 @@ contract Amazon {
     mapping (uint256 => Item) public items;
     event List(string name, uint256 cost, uint256 quantity);
     
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
     constructor(){
         owner = msg.sender;
     }
 
-    function list(uint256 _id, string memory _name, string memory _category, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public {
+    function list(uint256 _id, string memory _name, string memory _category, string memory _image, uint256 _cost, uint256 _rating, uint256 _stock) public onlyOwner {
         Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock );
         items[_id] = item; 
         emit List(_name, _cost, _stock);
+    }
+    function buy(uint256 _id) public payable {
         
-     }
+    }
 }
