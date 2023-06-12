9869948d3cae164316edb866e3279d91c12d72c6
talha073 <talhajavedd@gmail.com>
2023-06-12 12:54:07 +0400

    complete deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 const hre = require("hardhat");
+const { items } = require("./items.json");
 const token = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
@@ -9,6 +10,22 @@ async function main() {
   const amazon = await Amazon.deploy();
   await amazon.deployed();
   console.log(`amazon contract at ${amazon.address}`);
+
+  for (let i = 0; i < items.length; i++) {
+    const tx = await amazon
+      .connect(deployer)
+      .list(
+        items[i].id,
+        items[i].name,
+        items[i].category,
+        items[i].image,
+        token(items[i].price),
+        items[i].rating,
+        items[i].stock
+      );
+    await tx.wait();
+    console.log(`Listed items ${items[i].id}: ${items[i].name}`);
+  }
 }
 
 main().catch((error) => {
